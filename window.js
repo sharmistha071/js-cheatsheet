8db888dfc6231378cc4d77ede2258e38ebf730db
sharmistha071 <shormy.roy@gmail.com>
2018-04-30 10:44:58 +0600

    function declaration, loop, object and window added

---
+++ window.js
@@ -0,0 +1,29 @@
+//Alert
+//alert('hello world');
+
+//prompt
+// let input = prompt();
+// alert(input);
+
+//confirm
+if(confirm){
+  console.log('yes');
+}else{
+  console.log('no');
+}
+
+let val;
+
+//outer height and width
+
+val = window.outerHeight;
+val = window.outerWidth;
+
+//inner height and width
+val = window.innerHeight;
+val = window.innerWidth;
+
+//scroll
+val = window.scrollX;
+val = window.scrollY;
+console.log(val);
